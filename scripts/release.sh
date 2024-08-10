5681656c35822fc1facaa1d5ddd0e778f0d450ee
kcgidw <kc.gideonw@gmail.com>
2024-08-09 18:45:30 -0700

    readme in zip

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,6 +5,7 @@ rm -rf dist \
 && cp  kcvanilla.lua dist/kcvanilla.lua \
 && cp -r  jokers/ dist/jokers \
 && cp  lovely.toml dist/lovely.toml \
+&& cp  readme.md dist/readme.md \
 && rm -rf ~/AppData/Roaming/Balatro/Mods/kcvanilla \
 && cp -r dist ~/AppData/Roaming/Balatro/Mods/kcvanilla \
 && rm -f  kcvanilla-dist.zip \
