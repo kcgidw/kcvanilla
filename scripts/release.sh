40b0389e47a8ec54172d204b31d5855e4dbc3a61
kcgidw <kc.gideonw@gmail.com>
2025-01-05 15:35:52 -0800

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,20 @@
-rm -rf dist \
-&& mkdir -p dist/assets \
-&& cp -r assets/1x dist/assets \
-&& cp -r assets/2x dist/assets \
-&& cp  kcvanilla.json dist/kcvanilla.json \
-&& cp  kcvanilla.lua dist/kcvanilla.lua \
-&& cp -r  jokers/ dist/jokers \
-&& cp  lovely.toml dist/lovely.toml \
-&& cp  readme.md dist/readme.md \
-&& rm -rf ~/AppData/Roaming/Balatro/Mods/kcvanilla \
-&& cp -r dist ~/AppData/Roaming/Balatro/Mods/kcvanilla \
-&& rm -f  kcvanilla-dist.zip \
-&& 7z a kcvanilla-dist.zip dist
+# setup dist/
+rm -rf dist
+mkdir -p dist/assets
+
+# copy mod files to dist/
+cp -r assets/1x dist/assets
+cp -r assets/2x dist/assets
+cp  kcvanilla.json dist/kcvanilla.json
+cp  kcvanilla.lua dist/kcvanilla.lua
+cp -r  jokers/ dist/jokers
+cp  lovely.toml dist/lovely.toml
+cp  readme.md dist/readme.md
+
+# copy dist/ to Mods/
+rm -rf ~/AppData/Roaming/Balatro/Mods/kcvanilla
+cp -r dist ~/AppData/Roaming/Balatro/Mods/kcvanilla
+
+# zip dist/
+rm -f  kcvanilla-dist.zip
+7z a kcvanilla-dist.zip dist
