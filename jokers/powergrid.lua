ee6051eb97e94a638c29c31ee504abbc9084105d
jumbocarrot0 <jumbocarrot0@gmail.com>
2025-01-25 11:16:33 +1000

    Fixed Power Grid with latest SMOD

--- jokers/powergrid.lua
+++ jokers/powergrid.lua
@@ -1,3 +1,11 @@
+SMODS.Enhancement:take_ownership('m_mult', {
+    calculate = function(self, card, context)
+        if context.main_scoring then
+            G.GAME.current_round.kcv_mults_scored = (G.GAME.current_round.kcv_mults_scored or 0) + 1
+        end
+    end
+}, true)
+
 SMODS.Joker {
     key = "powergrid",
     name = "Power Grid",
@@ -19,34 +27,27 @@ SMODS.Joker {
     },
     loc_txt = {
         name = "Power Grid",
-        text = {'Scored {C:attention}Mult{} cards give {X:mult,C:white} X#1# {} Mult',
-                'for each {C:attention}Mult{} card scored this round',
-                "{C:inactive}(Next: {X:mult,C:white} X#2# {C:inactive} Mult)"}
+        text = { 'Scored {C:attention}Mult{} cards give {X:mult,C:white} X#1# {} Mult',
+            'for each {C:attention}Mult{} card scored this round',
+            "{C:inactive}(Next: {X:mult,C:white} X#2# {C:inactive} Mult)" }
     },
     loc_vars = function(self, info_queue, card)
         info_queue[#info_queue + 1] = G.P_CENTERS.m_mult
         local xmult = 1 + card.ability.extra + ((G.GAME.current_round.kcv_mults_scored or 0) * card.ability.extra)
         return {
-            vars = {card.ability.extra, xmult}
+            vars = { card.ability.extra, xmult }
         }
     end,
     calculate = function(self, card, context)
-        if context.setting_blind then
-            G.GAME.current_round.kcv_mults_scored = 0
-        end
         if context.individual and context.cardarea == G.play then
             local other = context.other_card
-            local other_enhancements = SMODS.get_enhancements(other)
-            if other_enhancements and other_enhancements.m_mult and not other.debuff then
+            if SMODS.has_enhancement(other, 'm_mult') and not other.debuff then
                 local xmult = 1 + ((G.GAME.current_round.kcv_mults_scored or 0) * card.ability.extra)
                 return {
                     x_mult = xmult,
                     card = context.blueprint_card or card
                 }
             end
-        end
-        if context.end_of_round then
-            G.GAME.current_round.kcv_mults_scored = 0
         end
     end
 }
