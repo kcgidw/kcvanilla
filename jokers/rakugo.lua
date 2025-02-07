8c0f281ddd781153fe750c40f288e0ae4233ff52
kcgidw <kc.gideonw@gmail.com>
2025-02-06 18:07:16 -0800

    rakugo: add guard

--- jokers/rakugo.lua
+++ jokers/rakugo.lua
@@ -17,7 +17,7 @@ SMODS.Joker {
         return {}
     end,
     calculate = function(self, card, context)
-        if context.repetition and context.other_card then
+        if context.repetition and context.other_card and context.poker_hands then
             if next(context.poker_hands["Straight"]) then
                 local rank = context.other_card:get_id()
                 if rank == 4 or rank == 5 or rank == 6 or rank == 7 or rank == 8 then
