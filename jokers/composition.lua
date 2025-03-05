353e3c737fbd9a9b84685ef800ac432a32bf5670
kcgidw <kc.gideonw@gmail.com>
2025-03-04 20:46:40 -0800

    nerf composition to 20 chips

--- jokers/composition.lua
+++ jokers/composition.lua
@@ -47,7 +47,7 @@ SMODS.Joker {
     config = {
         extra = {
             mult = 4,
-            chips = 25
+            chips = 20
         }
     },
     loc_vars = function(self, info_queue, card)
