9f49815cbd2152c5641a4d633b797bc3a98d359b
RainDance2871 <erik.nemmers@gmail.com>
2025-06-21 19:56:09 +0200

    Update composition.lua
    Indenting

--- jokers/composition.lua
+++ jokers/composition.lua
@@ -58,37 +58,37 @@ SMODS.Joker {
     end,
     
     calculate = function(self, card, context)
-    if context.joker_main then
-      local target_card = context.blueprint_card or card
-      local effect = kcv_composition_calc_effect(target_card, card.ability.extra.mult, card.ability.extra.chips)
-
-      local mult_message = nil
-      if effect.mult > 0 then
-        mult_message = {
-          mult_mod = effect.mult,
-          message = localize {
-              type = 'variable',
-              key = 'a_mult',
-              vars = {effect.mult}
-          },
-          colour = G.C.MULT
-        }
-      end
+        if context.joker_main then
+            local target_card = context.blueprint_card or card
+            local effect = kcv_composition_calc_effect(target_card, card.ability.extra.mult, card.ability.extra.chips)
         
-      if effect.chips > 0 then
-        return {
-          chip_mod = effect.chips,
-          message = localize {
-              type = 'variable',
-              key = 'a_chips',
-              vars = {effect.chips}
-          },
-          colour = G.C.CHIPS,
-          extra = mult_message
-        }
-      else
-        return mult_message
-      end
+            local mult_message = nil
+            if effect.mult > 0 then
+                mult_message = {
+                    mult_mod = effect.mult,
+                    message = localize {
+                        type = 'variable',
+                        key = 'a_mult',
+                        vars = {effect.mult}
+                    },
+                    colour = G.C.MULT
+                }
+            end
+            
+            if effect.chips > 0 then
+                return {
+                    chip_mod = effect.chips,
+                    message = localize {
+                        type = 'variable',
+                        key = 'a_chips',
+                        vars = {effect.chips}
+                    },
+                    colour = G.C.CHIPS,
+                    extra = mult_message
+                }
+            else
+                return mult_message
+            end
+        end
     end
-  end
 }
