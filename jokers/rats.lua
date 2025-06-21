297fcdd29f3403ccae7bfe5337eed807ab42a0c9
kcgidw <kc.gideonw@gmail.com>
2025-06-20 17:33:46 -0700

    rats: don't rat if only 1 card in hand

--- jokers/rats.lua
+++ jokers/rats.lua
@@ -22,30 +22,32 @@ SMODS.Joker {
         if context.first_hand_drawn and not context.blueprint then
             card.ability.can_spawn = false
 
-            local card_to_destroy = pseudorandom_element(G.hand.cards, pseudoseed('kcv_rats'))
-            if card_to_destroy then
+            if #G.hand.cards > 1 then
+                local card_to_destroy = pseudorandom_element(G.hand.cards, pseudoseed('kcv_rats'))
+                if card_to_destroy then
 
-                card_eval_status_text(card, 'extra', nil, nil, nil, {
-                    message = localize('kcv_rats_msg'),
-                    colour = G.C.FILTER,
-                    card = card
-                });
+                    card_eval_status_text(card, 'extra', nil, nil, nil, {
+                        message = localize('kcv_rats_msg'),
+                        colour = G.C.FILTER,
+                        card = card
+                    });
 
-                SMODS.destroy_cards(card_to_destroy)
+                    SMODS.destroy_cards(card_to_destroy)
 
-                delay(0.15)
-                G.E_MANAGER:add_event(Event({
-                    func = function()
-                        local has_room = #G.jokers.cards < G.jokers.config.card_limit
-                        if has_room then
-                            play_sound('timpani')
-                            local new_rat = copy_card(card, nil, nil, nil, card.edition and card.edition.negative)
-                            new_rat:add_to_deck()
-                            G.jokers:emplace(new_rat)
+                    delay(0.15)
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            local has_room = #G.jokers.cards < G.jokers.config.card_limit
+                            if has_room then
+                                play_sound('timpani')
+                                local new_rat = copy_card(card, nil, nil, nil, card.edition and card.edition.negative)
+                                new_rat:add_to_deck()
+                                G.jokers:emplace(new_rat)
+                            end
+                            return true
                         end
-                        return true
-                    end
-                }))
+                    }))
+                end
             end
 
             return true
